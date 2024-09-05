78890588b199a5d80b79210f12d70823db932aab
Starinin Andrey <starininandrey@gmail.com>
2024-09-05 21:05:49 +0300

    Initial commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfApp1;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(
+            caption: "Результат сохранения",
+            messageBoxText: $"{InputName.Text} {InputAge.Text}");
+    }
+
+    private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+    {
+        InputName.Clear();
+        InputAge.Clear();
+    }
+}
